ffb7aabb16fe713e82748f30896a3513a721c5e0
moehr1z <mooehr1z@gmail.com>
2023-08-15 22:44:31 +0200

    Enable printing of basic error message

--- nootboot.asm
+++ nootboot.asm
@@ -1,4 +1,44 @@
+%define DEBUG
+
 [bits 16]   ;tell assembler this is 16 bit code
 [org 0x7c00] ; tell assembler where code will be in memory after it was loaded (BIOS will load boot sector into memory at this address)
+
+%ifdef DEBUG
+    mov si, errStr
+%endif
+
+call print
+jmp $
+
+err: 
+%ifdef DEBUG
+    printC: ; print Character   
+        ; https://stanislavs.org/helppc/int_10-e.html
+        ; char in al
+        mov ah, 0x0e    ; writer character in tty mode
+        mov bx, 0x07    ; page nr0. + light grey on black
+
+        int 0x10
+        ret
+        
+
+    print: ; print null-terminated string
+        ; string location in si
+        mov al, [si]
+        and al, al
+        jz print.exit
+        call printC
+        inc si
+        jmp print
+    print.exit: 
+        ret
+
+%endif
+
+; Data
+%ifdef DEBUG
+    errStr db 'Ouch!', 0
+%endif
+
 TIMES 510 - ($ - $$) db 0	; fill the rest of sector with 0 ($ = pos beginning of line, $$ = beginning of current section, TIMES = assemble instruction N times)
 DW 0xAA55			; magic number
