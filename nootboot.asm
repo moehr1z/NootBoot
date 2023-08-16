34c743bcd952b7a4c2fb88eb7d8cd667153bcfc9
moehr1z <mooehr1z@gmail.com>
2023-08-16 22:49:10 +0200

    Refactor error printing

--- nootboot.asm
+++ nootboot.asm
@@ -12,15 +12,9 @@ jmp $
 
 err: 
 %ifdef DEBUG
-    printC: ; print Character   
-        ; https://stanislavs.org/helppc/int_10-e.html
-        ; char in al
-        mov ah, 0x0e    ; writer character in tty mode
-        mov bx, 0x07    ; page nr0. + light grey on black
-
-        int 0x10
-        ret
-        
+    mov si, errStr
+    call print
+    jmp $
 
     print: ; print null-terminated string
         ; string location in si
@@ -30,7 +24,16 @@ err:
         call printC
         inc si
         jmp print
-    print.exit: 
+    .exit: 
+        ret
+
+    printC: ; print Character   
+        ; https://stanislavs.org/helppc/int_10-e.html
+        ; char in al
+        mov ah, 0x0e    ; writer character in tty mode
+        mov bx, 0x07    ; page nr0. + light grey on black
+
+        int 0x10        ; (the IVT was already filled by the BIOS)
         ret
 
 %endif
