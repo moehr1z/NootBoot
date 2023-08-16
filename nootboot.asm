80ac4d7da6881291d6edcc4651243d9cbe76f251
moehr1z <mooehr1z@gmail.com>
2023-08-16 22:49:56 +0200

    Add code to check if LBA works

--- nootboot.asm
+++ nootboot.asm
@@ -4,11 +4,22 @@
 [org 0x7c00] ; tell assembler where code will be in memory after it was loaded (BIOS will load boot sector into memory at this address)
 
 %ifdef DEBUG
-    mov si, errStr
+    call checkFuncs
 %endif
 
 call print
 jmp $
+%ifdef DEBUG
+checkFuncs: ; functions to check if certain hardware features are available
+    checkFuncs.LBA: ; check if LBA adressing works
+        mov ah, 0x41
+        mov bx, 0x55aa
+        mov dl, 0x80
+        int 0x13
+        jc err
+    
+    ret
+%endif
 
 err: 
 %ifdef DEBUG
