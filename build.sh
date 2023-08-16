3a10701af028638eacc4d81cc37215279d984988
moehr1z <mooehr1z@gmail.com>
2023-08-16 23:28:47 +0200

    Add concatenation of kernel and initrd to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-nasm nootboot.asm -o NootBoot
+OUTPUT="NootBoot"
+
+KERNEL="kernel"
+KERNEL_SIZE=$(stat -c %s $KERNEL)
+KERNEL_PAD=$((512 - ($KERNEL_SIZE % 512)))  # padding needed for sector alignement
+
+INITRD="initrd"
+INITRD_SIZE=$(stat -c %s $INITRD)
+INITRD_PAD=$((512 - ($INITRD_SIZE % 512)))  # padding needed for sector alignement
+
+nasm nootboot.asm -o $OUTPUT
+
+# concatenate kernel and fill up to whole sector
+cat "$KERNEL" >> "$OUTPUT"
+dd if=/dev/zero bs=1 count=$KERNEL_PAD >> "$OUTPUT"
+
+# concatenate intird and fill up to whole sector
+cat "$INITRD" >> "$OUTPUT"
+dd if=/dev/zero bs=1 count=$INITRD_PAD >> "$OUTPUT"
