d5e6f17e05455d74b2f98536af4fa343b6cbc93d
moehr1z <mooehr1z@gmail.com>
2023-08-16 22:50:41 +0200

    Add code to read from disk

--- nootboot.asm
+++ nootboot.asm
@@ -9,6 +9,32 @@
 
 call print
 jmp $
+
+diskRead:   ; read from disk, using LBA adressing
+    ; .count = ax, .offset = bx, .segment = cx
+    ; has to be called from unreal mode or protected mode
+    
+    push edx    ; save edx
+
+    ; setup disk address packet structure
+    mov edx, [curLBA]
+    mov [DAPACK.count], ax
+    mov [DAPACK.offset], bx
+    mov [DAPACK.segment], cx
+    mov [DAPACK.lba], edx
+    add [curLBA], ax
+    
+    ; actually read
+    mov si, DAPACK
+    mov ah, 0x42
+    mov dl, 0x80    ; first drive
+    int 0x13
+    jc err
+
+    pop edx     ; restore edx 
+
+    ret
+    
 %ifdef DEBUG
 checkFuncs: ; functions to check if certain hardware features are available
     checkFuncs.LBA: ; check if LBA adressing works
@@ -53,6 +79,17 @@ err:
 %ifdef DEBUG
     errStr db 'Ouch!', 0
 %endif
+
+DAPACK: ; Disk Adress Packet Structure
+            db 0x10     ; size of packet (16 bytes)
+            db 0x0      ; always 0
+.count      dw 0        ; nr of sectors to transfer
+.offset     dw 0        ; transfer buffer offset
+.segment    dw 0        ; transfer buffer segment
+.lba        dd 0        ; lower 32-bits of 48-bit starting LBA 
+            dd 0        ; upper 32-bits of 48-bit starting LBA (we won't need them)
+
+curLBA dd 0x0           ; holds current LBA (TODO set this to kernel position)
 
 TIMES 510 - ($ - $$) db 0	; fill the rest of sector with 0 ($ = pos beginning of line, $$ = beginning of current section, TIMES = assemble instruction N times)
 DW 0xAA55			; magic number
